8029a7864a16024692386f770b4858a03e48e9b7
Daffa <affah2212@gmail.com>
2026-04-02 22:20:33 +0700

    feat: implement filter and sort functionality

--- src/script/app.js
+++ src/script/app.js
@@ -1,15 +1,24 @@
-import { state, cart, setProducts,setFilter,applyFilters,saveCart,loadCart } from "./state.js";
-import renderProducts from './render.js'
+import {
+  state,
+  cart,
+  setProducts,
+  setFilter,
+  applyFilters,
+  saveCart,
+  loadCart,
+} from './state.js';
+import renderProducts from './render.js';
 
 async function initApp() {
   try {
-    const res = await fetch('/src/data/products.json')
-    if(!res.ok) throw new Error('Failed to load data');
+    const res = await fetch('/src/data/products.json');
+    if (!res.ok) throw new Error('Failed to load data');
 
-    const data = await res.json()
-    setProducts(data)
-    loadCart()
-    renderProducts()
+    const data = await res.json();
+    setProducts(data);
+    loadCart();
+    renderProducts();
+    initFilters();
 
     console.log(state);
   } catch (err) {
@@ -17,5 +26,30 @@ async function initApp() {
   }
 }
 
-initApp()
+function initFilters() {
+  document.getElementById('filter-category').addEventListener('change', (e) => {
+    const value = e.target.value;
+    setFilter('category', value);
+    applyFilters();
+    renderProducts();
+  });
+  document.getElementById('filter-price').addEventListener('input', (e) => {
+    const value = Number(e.target.value);
+    document.getElementById('price-output').innerText = value.toLocaleString('id-ID', {
+      style : 'currency',
+      currency : "IDR",
+      minimumFractionDigits : 0,
+    })
+    setFilter('maxPrice', value);
+    applyFilters();
+    renderProducts();
+  });
+  document.getElementById('sort-by').addEventListener('change', (e) => {
+    const value = e.target.value;
+    setFilter('sortBy', value);
+    applyFilters();
+    renderProducts();
+  });
+}
 
+initApp();
