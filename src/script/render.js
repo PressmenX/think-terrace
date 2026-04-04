c984b3aedfffe03f98d730ae113f721458036963
Daffa <affah2212@gmail.com>
2026-04-04 22:35:41 +0700

    style: finished styling and responsive

--- src/script/render.js
+++ src/script/render.js
@@ -10,21 +10,32 @@ const toRupiah = (num) => {
 
 export function renderProducts() {
   const container = document.getElementById('product-grid');
-  container.innerHTML = state.filtered
-    .map((p) => {
-      const isAdded = state.cart.some((item) => item.id === p.id);
-      
-      return `<div class="product-item" data-id="${p.id}">
+
+  if (state.filtered.length > 0) {
+    container.innerHTML = state.filtered
+      .map((p) => {
+        const isAdded = state.cart.some((item) => item.id === p.id);
+
+        return `<div class="product-item" data-id="${p.id}">
         <img src="${p.image}" alt="">
-        <h3 class="item-name">${p.name}</h3>
-        <h4 class="item-series">${p.series} Series</h4>
-        <h2 class="item-price">${toRupiah(p.price)}</h2>
-        <span class="item-rating">⭐${p.rating}</span>
-        <button class="add-cart-btn" ${isAdded ? 'disabled' : ''}>
-        ${isAdded ? 'Added✓' : 'Add to Cart'}</button>
+        <div class="product-info">
+          <h4 class="item-name">${p.name}</h4>
+          <h4 class="item-series">${p.series} Series</h4>
+          <h3 class="item-price">${toRupiah(p.price)}</h3>
+        </div>
+        <div class="product-footer">
+          <span class="item-rating"><span>★</span>${p.rating}</span>
+          <button class="add-cart-btn" ${isAdded ? 'disabled' : ''}>
+          ${isAdded ? '✓ Added' : '+ Add to Cart'}</button>
+        </div>
       </div>`;
-    })
-    .join('');
+      })
+      .join('');
+  } else {
+    container.innerHTML = `<div id="no-match">No products match your criteria.
+
+</div>`
+  }
 }
 
 export function renderCart() {
@@ -34,14 +45,18 @@ export function renderCart() {
       .map((p) => {
         return `
       <li class="cart-item" data-id="${p.id}">
-        <button class="remove-cart-item">🗑️</button>
+        <button class="remove-cart-item"><span class="material-symbols-outlined delete-icon">
+              delete
+        </span></button>
         <img src="${p.image}" alt="">
-        <h2 class="cart-item-name">${p.name}</h2>
-        <h2 class="cart-item-price">${toRupiah(p.price)}</h2>
+        <div class="cart-info">
+          <h4 class="cart-item-name">${p.name}</h4>
+          <p class="cart-item-price">${toRupiah(p.price)}</p>
+        </div>
         <div class="control-qty">
-        <button class="decrease-qty">-</button>
-        <span class="qty-count">${p.quantity}</span>
-        <button class="increase-qty">+</button>
+          <button class="decrease-qty">-</button>
+          <span class="qty-count">${p.quantity}</span>
+          <button class="increase-qty">+</button>
         </div>
         <span class="cart-item-subtotal">Subtotal: ${toRupiah(p.price * p.quantity)}</span>
       </li>
@@ -49,12 +64,20 @@ export function renderCart() {
       })
       .join('');
   } else {
-    cartList.innerHTML = `<p id="no-item-cart">Your cart is empty</p>`;
+    cartList.innerHTML = `
+    <div id="no-item-cart">
+      <span class="material-symbols-outlined cart-icon">shopping_cart</span>
+      <p>Your cart is empty</p>
+    </div>`;
   }
 
   document.getElementById('cart-total').innerText = toRupiah(cart.getTotal());
 }
 
 export function updateCartBadge() {
-  document.getElementById('cart-badge').innerText = Number(cart.getCount());
+  const badge = document.getElementById('cart-badge');
+  if (state.cart.length > 0) {
+    badge.classList.add('show');
+    badge.innerText = Number(cart.getCount());
+  } else badge.classList.remove('show');
 }
